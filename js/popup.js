32fe7a0194c22fd19b951513144adf7acbc13a2a
Gustavo Souza <135669233+Guuzin@users.noreply.github.com>
2023-06-28 21:28:45 -0300

    Master Animefan

---
+++ js/popup.js
@@ -0,0 +1,14 @@
+// const popup1 = document.querySelector("#popup1");
+// const fechaPopup = document.querySelector('.fechaPopup')
+
+// window.onload = function () {
+
+//   popup1.show();
+
+// };
+
+
+// fechaPopup.addEventListener('click', (e)=> {
+//   e.preventDefault()
+//   popup1.close()
+// }) 
